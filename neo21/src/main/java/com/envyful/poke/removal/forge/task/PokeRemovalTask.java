703b94464e5eb1e8ad68f4ecb3825155df6afb63
Daniel Norris <dnorris443@gmail.com>
2025-08-06 14:52:48 +0100

    feat(project): update to 1.21.1

--- neo21/src/main/java/com/envyful/poke/removal/forge/task/PokeRemovalTask.java
+++ neo21/src/main/java/com/envyful/poke/removal/forge/task/PokeRemovalTask.java
@@ -1,16 +1,15 @@
 package com.envyful.poke.removal.forge.task;
 
-import com.envyful.api.neoforge.chat.UtilChatColour;
 import com.envyful.api.neoforge.world.UtilWorld;
+import com.envyful.api.platform.PlatformProxy;
+import com.envyful.api.text.Placeholder;
 import com.envyful.poke.removal.forge.PokeRemovalForge;
 import com.envyful.poke.removal.forge.config.PokeRemovalConfig;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -20,30 +19,29 @@ public class PokeRemovalTask implements Runnable {
     private final PokeRemovalForge mod;
     private final Map<String, Long> lastRemoval = Maps.newHashMap();
 
-    public PokeRemovalTask(PokeRemovalForge mod) {this.mod = mod;}
+    public PokeRemovalTask(PokeRemovalForge mod) {
+        this.mod = mod;
+    }
 
     @Override
     public void run() {
         List<PokeRemovalConfig.RemovalSetting> toRemove = Lists.newArrayList();
 
-        for (PokeRemovalConfig.RemovalSetting value : this.mod.getConfig().getRemovalSettings().values()) {
+        for (var value : this.mod.getConfig().getRemovalSettings().values()) {
             long time = TimeUnit.MINUTES.toMillis(value.getRemovalTimeMinutes());
             long lastRemovalDuration = (this.getLastRemoval(value) + time) - System.currentTimeMillis();
 
-            if (lastRemovalDuration <= 0) {
+            if (lastRemovalDuration <= 0 && !value.isLocked()) {
                 toRemove.add(value);
             }
 
-            for (PokeRemovalConfig.WarningBroadcast warningBroadcast : value.getWarningBroadcasts().values()) {
+            for (var warningBroadcast : value.getWarningBroadcasts().values()) {
                 if (!warningBroadcast.isEnabled()) {
                     continue;
                 }
 
                 if (TimeUnit.MILLISECONDS.toSeconds(lastRemovalDuration) == warningBroadcast.getTimeBeforeRemovalSeconds()) {
-                    for (String s : warningBroadcast.getBroadcast()) {
-                        ServerLifecycleHooks.getCurrentServer().getPlayerList()
-                                .broadcastSystemMessage(UtilChatColour.colour(s), false);
-                    }
+                    PlatformProxy.broadcastMessage(warningBroadcast.getBroadcast());
                 }
             }
         }
@@ -52,24 +50,16 @@ public class PokeRemovalTask implements Runnable {
             return;
         }
 
-        for (PokeRemovalConfig.RemovalSetting removalSetting : toRemove) {
-            if (removalSetting.isLocked()) {
-                continue;
-            }
-
+        for (var removalSetting : toRemove) {
             int removed = 0;
             removalSetting.setLocked(true);
 
-            for (ServerLevel world : ServerLifecycleHooks.getCurrentServer().getAllLevels()) {
+            for (var world : ServerLifecycleHooks.getCurrentServer().getAllLevels()) {
                 if (removalSetting.getBlacklistedWorlds().contains(UtilWorld.getName(world))) {
                     continue;
                 }
 
-                Iterator<Entity> iterator = world.getEntities().getAll().iterator();
-
-                while (iterator.hasNext()) {
-                    Entity next = iterator.next();
-
+                for (Entity next : world.getEntities().getAll()) {
                     if (next != null && removalSetting.shouldRemove(next)) {
                         next.kill();
                         ++removed;
@@ -83,16 +73,9 @@ public class PokeRemovalTask implements Runnable {
                 continue;
             }
 
-            for (String s : removalSetting.getRemovalBroadcast()) {
-                ServerLifecycleHooks.getCurrentServer().getPlayerList().broadcastSystemMessage(UtilChatColour.colour(s.replace(
-                        "%amount%",
-                        removed + ""
-                )), false);
-            }
-
+            PlatformProxy.broadcastMessage(removalSetting.getRemovalBroadcast(), Placeholder.simple("%amount%", removed + ""));
             removalSetting.setLocked(false);
         }
-
     }
 
     public long getLastRemoval(PokeRemovalConfig.RemovalSetting setting) {
